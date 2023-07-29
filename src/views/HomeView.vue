4bf967d88fd2ee74f60b227452a939ef5fcf5cf2
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-29 08:58:51 +0800

    views

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: '',
+  }
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+</style>
