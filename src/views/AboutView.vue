91ad5706abf3dabed696e63b3132feeb9649eaf7
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-29 12:01:17 +0800

    test

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: 'AboutView',
+  }
+</script>
+
+<template>
+  <div>
+    ABout
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+
+  div {
+    min-height: 100vh;
+  }
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: 'ContactView',
+  }
+</script>
+
+<template>
+  <div>
+    contgacty
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+
+  div {
+    min-height: 100vh;
+  }
+</style>
