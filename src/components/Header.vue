72366fc77c87dd5f831154d871ae66ea6c341ad5
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-29 19:02:03 +0800

    desktop responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,4 @@
 <script>
-  import { RouterLink } from 'vue-router'
   export default {
     name: 'Header',
     data() {
@@ -18,7 +17,7 @@
             link: '#projects'
           },
           {
-            name: 'contact',
+            name: 'Contact',
             link: '#contact'
           }
         ],
@@ -52,8 +51,12 @@
       :class="{ open: isOpen }"
       >
       <ul class="header__list">
-        <li class="header__item" v-for="nav in nav_list" :key="nav.name">
-          <a :href="nav.link">{{ nav.name }}</a>
+        <li 
+          v-for="(nav, index) in nav_list" 
+          :key="nav.name"
+          class="header__item" 
+        >
+          <a :href="nav.link" class="header__link">{{ nav.name }}</a>
         </li>
       </ul>
     </nav>
@@ -70,6 +73,7 @@
     align-items: center;
     justify-content: space-between;
     padding-block: rem(16);
+    z-index: 10;
 
     &__logo {
       font-size: clampf(24, 375, 899, 32);
@@ -94,24 +98,83 @@
 
     &__nav {
       position: absolute;
-      right: -100%;
-      top: 100%;
+      right: 0%;
+      top: -300%;
       background-color: #444;
       padding: rem(32);
-      width: 30%;
+      width: 100%;
       overflow: hidden;
+      z-index: -1;
     }
 
     &__list {
       display: flex;
-      align-items: end;
+      align-items: center;
       justify-content: center;
       flex-direction: column;
     }
 
     &__item {
+
       &:not(:last-child) {
         padding-bottom: rem(16);
+      }
+    }
+
+    &__link {
+      color: white;
+    }
+
+    @include breakpointMin(medium-screen) {
+      outline: $outline;
+      &__button {
+        display: none;
+      }
+
+      &__nav {
+        position: unset;
+        background-color: unset;
+        padding: 0;
+        width: auto;
+        overflow: visible;
+      }
+
+      &__list {
+        align-items: unset;
+        flex-direction: row;
+        gap: rem(32);
+      }
+
+      &__item {
+        &:not(:last-child) {
+          padding-bottom: 0;
+        }
+      }
+
+      &__link {
+        position: relative;
+        z-index: 1;
+
+        &::before {
+          content: '';
+          display: block;
+          width: 0;
+          height: 5px;
+          background-color: $link;
+          margin-inline: auto;
+
+          position: absolute;
+          bottom: 1px;
+          right: 0;
+          left: 0;
+          z-index: -1;
+
+          transition: width 300ms ease-out;
+        }
+
+        &:hover::before {
+          width: 100%;
+        }
       }
     }
   }
