61428569ecd6087f1143722a2ccc345c9223c7fb
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-29 08:58:45 +0800

    pinia store

---
+++ src/store/PortfolioStore.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+
+export const usePortfolioStore = defineStore('portfolio-store', {
+  state: () => {
+    // states here
+    return {  }
+  },
+  getters: {
+    // getters here
+  },
+  actions: {
+    // actions here
+  }
+})
