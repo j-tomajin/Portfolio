a431250c30c76ddbcf67b2c3fc146fb0736425c2
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-29 12:00:49 +0800

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script>
+  import { RouterView } from 'vue-router'
+  import Header from './components/Header.vue';
   export default {
     name: 'App',
+    components: { Header }
   }
 </script>
 
 <template>
+  <Header></Header>
   <main>
     <h1>HELLO WORLD</h1>
   </main>
+  
+<!-- 
+  <section id="about">about</section>
+  <section id="projects">projects</section>
+  <section id="contact">contact</section> -->
 </template>
 
 <style lang="scss" scoped>
