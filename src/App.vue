d878060dac23df92003c173de5f887ae4f59fe1d
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-29 08:58:54 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<template>
+  <main>
+    <h1>HELLO WORLD</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  @use './assets/scss/utilities/' as *;
+</style>
